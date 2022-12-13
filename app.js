4f8270c9c18f33978e11b8f05aa9c7dd2040bdf5
Amina HAKIMI <95310769+Samarcande86@users.noreply.github.com>
2022-12-13 11:38:37 +0100

    add middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { hashPassword } = require("./auth.js");
 const express = require("express");
 const { validateUser, validateMovie } = require("./validators.js");
 const app = express();
@@ -17,13 +18,14 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 const userHandlers = require("./userHandlers");
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", userHandlers.postUser);
 app.put("/api/users/:id", userHandlers.updateUser);
-
+app.delete("/api/users/:id", userHandlers.deleteUser);
+app.post("/api/users", hashPassword, userHandlers.postUser);
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
--- userHandlers.js
+++ userHandlers.js
@@ -1,8 +1,35 @@
 const database = require("./database");
 
+const database = require("./database");
+
 const getUsers = (req, res) => {
+  const initialSql = "select * from users";
+  const where = [];
+
+  if (req.query.city != null) {
+    where.push({
+      column: "city",
+      value: req.query.city,
+      operator: "=",
+    });
+  }
+  if (req.query.language != null) {
+    where.push({
+      column: "language",
+      value: req.query.language,
+      operator: "=",
+    });
+  }
+
   database
-    .query("select * from users")
+    .query(
+      where.reduce(
+        (sql, { column, operator }, index) =>
+          `${sql} ${index === 0 ? "where" : "and"} ${column} ${operator} ?`,
+        initialSql
+      ),
+      where.map(({ value }) => value)
+    )
     .then(([users]) => {
       res.json(users);
     })
@@ -29,13 +56,16 @@ const getUserById = (req, res) => {
       res.status(500).send("Error retrieving data from database");
     });
 };
+
 const postUser = (req, res) => {
-  const { firstname, lastname, email, city, language} = req.body;
+  console.log(req.body);
+  const { firstname, lastname, email, city, language, hashedPassword } =
+  req.body;
 
   database
     .query(
-      "INSERT INTO movies(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
-      [firstname, lastname, email, city, language]
+      "INSERT INTO users(firstname, lastname, email, city, language, hashedPassword) VALUES (?, ?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language, hashedPassword]
     )
     .then(([result]) => {
       res.location(`/api/users/${result.insertId}`).sendStatus(201);
@@ -45,6 +75,7 @@ const postUser = (req, res) => {
       res.status(500).send("Error saving the user");
     });
 };
+
 const updateUser = (req, res) => {
   const id = parseInt(req.params.id);
   const { firstname, lastname, email, city, language } = req.body;
@@ -66,9 +97,29 @@ const updateUser = (req, res) => {
       res.status(500).send("Error editing the user");
     });
 };
+
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from users where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the user");
+    });
+};
+
 module.exports = {
   getUsers,
   getUserById,
   postUser,
   updateUser,
+  deleteUser,
 };
